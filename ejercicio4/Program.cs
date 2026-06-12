bbb9665c5b79bc5c434568359ecc36758006c39e
Taxcher-tk07 <luispracticas0703@gmail.com>
2026-06-12 16:19:31 -0700

    Actualizacion del proyecto

---
+++ ejercicio4/Program.cs
@@ -0,0 +1,47 @@
+﻿Random dice = new Random();
+
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+
+/*int roll1 = 6;
+int roll2 = 6;
+int roll3 = 6;*/
+
+int total = roll1 + roll2 + roll3;
+
+Console.Clear();
+Console.WriteLine($"\nDice roll: {roll1} + {roll2} + {roll3} = {total}\n");
+
+if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("Ojooo tres en racha, mas puntos en camino!!!\n");
+        total += 6;
+    }else{
+        Console.WriteLine("Obtuviste puntos extra hijo!!!\n");
+        total += 2;
+    }
+}     
+
+if(total >= 16)
+{
+    Console.WriteLine($"Ganaste un auto nuevo!\n");
+    Console.WriteLine($"Nuevo total: {total}\n");
+}
+else if(total >= 10)
+{
+    Console.WriteLine($"Ganaste una Laptop nueva!\n");
+    Console.WriteLine($"Nuevo total: {total}\n");
+}
+else if(total >= 7)
+{
+    Console.WriteLine("Ganaste un viaje todo pagado!\n");
+    Console.WriteLine($"Nuevo total: {total}\n");
+}
+else
+{
+    Console.WriteLine("Ganaste un gatito :D");
+    Console.WriteLine($"Nuevo total: {total}\n");
+}
---
+++ ejercicio5/Program.cs
@@ -0,0 +1,37 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+Console.Clear();
+if(daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+}
+else if(daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+}
+
+
+if(daysUntilExpiration == 0)
+{
+    Console.WriteLine("\nTu suscripcion ha expirado");
+}
+else if(daysUntilExpiration == 1)
+{
+    Console.WriteLine(@$"
+Tu suscripcion expira mañana
+renueva ahora y obten {discountPercentage}% de descuento!");
+}
+else if(daysUntilExpiration <= 5)
+{
+    Console.WriteLine(@$"
+Tu suscripcion expira en {daysUntilExpiration} dias!
+renueva ahora y obten {discountPercentage}% de descuento!");
+}
+else if(daysUntilExpiration <= 10)
+{
+    Console.WriteLine("\nTu suscripcion expirara pronto!");
+}
+
+Console.WriteLine(daysUntilExpiration == 0 ? "\nExpiró" : "\nQuedan " + daysUntilExpiration + " dias restantes\n");
